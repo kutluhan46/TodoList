395bbe568d882f5180237d8f09ed4ae78b268876
kutluhan46 <kutluhanakyol21@gmail.com>
2022-11-04 15:25:34 +0300

    flex eklendi

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
+let list=[]
 let addBTN= document.getElementById('addToDo');
 let toDoContainer= document.getElementById('toDoContainer');
 let txtText=document.getElementById('txtText')
 let clearBtn=document.getElementById('clearToDo')
+let consoleBas=document.getElementById('consoleBas')
 
 addBTN.addEventListener('click', function(){
     let parag = document.createElement('p')
     parag.classList.add('parag-style')
     toDoContainer.appendChild(parag);
+    list.push(txtText.value)
     parag.innerHTML=txtText.value;
     txtText.value="";
-
+    
     parag.addEventListener('click', function(){
         parag.style.textDecoration = 'line-through';
     })
     parag.addEventListener('dblclick', function(){
         toDoContainer.removeChild(parag)
+        list.splice()
+        
     })
 
     clearBtn.addEventListener('click', function(){
         parag.remove()
+        list=[]
+        
+       
+    })
+    consoleBas.addEventListener('click', function(){
+       console.log(list)
+        
+       
     })
 })
